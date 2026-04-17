878c627021b17fecaabee38e36873d0394c62aa8
Abtahi Islam <143752829+Abtahi360@users.noreply.github.com>
2026-04-18 03:29:13 +0600

    Add directed and undirected graph implementations with input files

---
+++ weighted graph suing matrix_Undirected graphs/weighted graph suing matrix_Undirected graphs.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n, m;
+
+    ifstream fin("g1.txt");
+    fin >> n >> m;
+
+    int **g = new int*[n];
+    for (int i = 0; i < n; i++) {
+        g[i] = new int[n];
+    }
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            g[i][j] = 99999;
+        }
+    }
+
+    int u, v, w;
+    for (int i = 0; i < m; i++) {
+        fin >> u >> v >> w;
+        g[u][v] = g[v][u] = w;
+    }
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (g[i][j] == 99999)
+                cout << "INF ";
+            else
+                cout << g[i][j] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
